4f85d14b2deca056ffc85e90be2e9f25e49134eb
hayoonida <58282987+hayoonida@users.noreply.github.com>
2019-12-06 23:38:21 +0900

    Create 2742.cpp

---
+++ 2742.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+
+	int N = 0;
+	cin >> N;
+	for (int i = N; i >0; i--) {
+		cout << i<< "\n";
+	}
+}
