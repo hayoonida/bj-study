23297f6af70c247b84f3e27997fe85ec517d99a4
hayoonida <58282987+hayoonida@users.noreply.github.com>
2019-12-09 18:40:14 +0900

    Create 2445.cpp

---
+++ 2445.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	int N;
+	cin >> N;
+
+	for (int i = 1; i <= N; i++) {
+		for (int a = 1; a <= i; a++) {
+			cout << "*";
+		}
+		for (int a = i; a < N; a++) {
+			cout << "  ";
+		}
+		for (int a = 1; a <= i; a++) {
+			cout << "*";
+		} 
+		cout << "\n";
+	}////////// n번째줄까지임
+
+	for (int i = 1; i < N;i++) {
+
+		for (int a = i; a < N;a++) {
+			cout<<"*";
+		}
+		for (int a = 1; a <=i; a++) {
+			cout << "  ";
+		}
+		for (int a = i; a < N; a++) {
+			cout << "*";
+		}
+
+
+		cout << "\n";
+	}
+
+
+}
